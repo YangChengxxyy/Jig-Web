21da22b72448c31f560814330902891bb182e41c
YangChengxxyy <853364106@qq.com>
2020-05-18 14:50:29 +0800

    高级用户 我的采购单 完成

--- src/views/high/purchase/my/index.vue
+++ src/views/high/purchase/my/index.vue
@@ -1,6 +1,6 @@
 <template>
   <el-card class="box-card">
-    <el-button type="primary" style="margin-left: 3%;margin-top: 2%" @click="drawerVisible = true">
+    <el-button type="primary" style="margin-left: 3%;margin-top: 2%" @click="showPurchase('添加采购单')">
       添加采购单
     </el-button>
     <el-table :data="purchase_income_submit_list" border style="width: 94%; margin: 1% 3% 2%">
@@ -23,7 +23,7 @@
       </el-table-column>
       <el-table-column label="操作" width="100%">
         <template slot-scope="scope">
-          <el-button type="text" :disabled="scope.row.status !== '0'" @click="showRepair(scope.row)">修改</el-button>
+          <el-button type="text" :disabled="scope.row.status !== '0'" @click="showPurchase('修改采购单',scope.row)">修改</el-button>
           <el-popconfirm
             confirm-button-text="确定"
             cancel-button-text="不用"
@@ -48,32 +48,32 @@
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
     />
-    <el-drawer ref="drawer" title="添加采购单" :visible.sync="drawerVisible" direction="rtl" :size="'550px'">
-      <el-form label-width="100px" label-position="left">
+    <el-drawer ref="drawer" :title="drawerTitle" :visible.sync="drawerVisible" direction="rtl" :size="'550px'" @before-close="clean('add')">
+      <el-form ref="add" label-width="100px" label-position="left" :model="form">
         <el-row>
           <el-col :span="20" :offset="2">
-            <el-form-item label="单据号">
-              <el-input v-model="form.bill_no" readonly />
+            <el-form-item label="单据号" :rules="[{required: true,message:'请填写单据号',trigger: 'blur'}]">
+              <el-input v-model="form.bill_no" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="20" :offset="2">
-            <el-form-item label="采购人id">
-              <el-input v-model="form.submit_id" readonly />
+            <el-form-item label="采购人id" :rules="[{required: true,message:'请填写采购人id',trigger: 'blur'}]">
+              <el-input v-model="form.submit_id" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row :gutter="20">
-          <el-col :span="10" :offset="2">
-            <el-form-item label="单据号">
+          <el-col :span="11" :offset="2">
+            <el-form-item label="产线" :rules="[{required: true,message:'请选择产线',trigger: 'blur'}]">
               <el-select v-model="form.production_line_id">
                 <el-option v-for="item in production_line_list" :key="item.id" :value="item.id" :label="item.name" />
               </el-select>
             </el-form-item>
           </el-col>
-          <el-col :span="5" :offset="5">
-            <el-button type="danger" icon="el-icon-delete">清 空</el-button>
+          <el-col :span="5" :offset="4">
+            <el-button type="danger" icon="el-icon-delete" @click="clean('add')">清 空</el-button>
           </el-col>
         </el-row>
         <el-row>
@@ -81,20 +81,40 @@
             <el-divider />
           </el-col>
         </el-row>
-        <el-row :gutter="10">
+        <el-row v-for="(item,index) in form.code" :key="index" :gutter="10">
           <el-col :span="9" :offset="2">
-            <el-form-item label="工夹具代码" label-width="90px">
-              <el-input />
+            <el-form-item :label="'工夹具代码'+(index+1)" label-width="100px" :rules="[{required:true,message:'此项不得为空',trigger:'blur'}]">
+              <el-select v-model="form.code[index]">
+                <el-option v-for="item2 in code_list" :key="item2" :value="item2" />
+              </el-select>
             </el-form-item>
           </el-col>
-          <el-col :span="6">
-            <el-form-item label="数量" label-width="40px" style="width: 100%">
-              <el-input-number v-model="number" controls-position="right" size="medium" style="width: 100%" />
+          <el-col :span="7">
+            <el-form-item
+              label="数量"
+              label-width="50px"
+              style="width: 100%"
+              :rules="[
+                {required: true,message :'此项不得为空',trigger: 'blur'},
+                {min: 1,message: '不得小于1',trigger: 'change'}]"
+            >
+              <el-input-number v-model="form.count[index]" controls-position="right" size="medium" style="width: 100%" />
             </el-form-item>
           </el-col>
           <el-col :span="5">
-            <el-button icon="el-icon-minus" circle />
-            <el-button icon="el-icon-plus" circle />
+            <el-button icon="el-icon-minus" circle @click="reduce(index)" />
+            <el-button v-if="index === form.code.length-1" icon="el-icon-plus" circle @click="add" />
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="20" :offset="2">
+            <el-divider />
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="20" :offset="2">
+            <el-button style="width: 48%" @click="cancel('drawer')">取 消</el-button>
+            <el-button type="primary" style="width: 48%" @click="submitPurchase(drawerTitle)">确 定</el-button>
           </el-col>
         </el-row>
       </el-form>
@@ -113,15 +133,19 @@ export default {
       page_number: 1,
       page_size: 10,
       all: 0,
+      all_code_list: [],
       code_list: [],
       drawerVisible: false,
       production_line_list: [],
       form: {
+        id: '',
         bill_no: '',
         submit_id: '',
-        production_line_id: ''
+        production_line_id: '',
+        code: [''],
+        count: [1]
       },
-      number: 0
+      drawerTitle: ''
     }
   },
   computed: {
@@ -129,11 +153,32 @@ export default {
       'id'
     ])
   },
+  watch: {
+    form: {
+      handler(newValue, oldValue) {
+        this.code_list = this.all_code_list
+        newValue.code.forEach(
+          (v, i) => {
+            this.code_list = this.code_list.filter(
+              (item) => {
+                return item !== v
+              })
+          })
+      },
+      deep: true
+    }
+  },
   created() {
     this.getData()
     this.$ajax('/get_production_line_list').then(
       res => {
         this.production_line_list = res.data
+      }
+    )
+    this.$ajax('/get_code_list').then(
+      res => {
+        this.all_code_list = res.data
+        this.code_list = res.data
       }
     )
   },
@@ -152,8 +197,18 @@ export default {
         }
       )
     },
-    showRepair: function(row) {
-      this.form = row
+    showPurchase: function(title, row = { bill_no: '', submit_id: '', production_line_id: '', code: [''], count: [1] }) {
+      this.drawerTitle = title
+      if (title === '添加采购单') {
+        this.form = row
+      } else {
+        const a = JSON.parse(JSON.stringify(row))// 防止指针错误
+        const { code, count } = a
+        // const { code, count } = row
+        this.form = a
+        this.form.code = code.split('|')
+        this.form.count = count.split('|')
+      }
       this.drawerVisible = true
     },
     /**
@@ -183,48 +238,58 @@ export default {
         }
       )
     },
-    submitRepair(formName, upload) {
-      this.$refs[formName].validate(
-        valid => {
-          if (valid) {
-            this.$confirm('是否提交').then(
-              _ => {
-                this.submitUpload()
-                this.$refs['drawer'].closeDrawer()
-                this.$refs[upload].clearFiles()
-                this.$refs[formName].resetFields()
-                this.getData()
-              }
-            ).catch(
-              _ => {
-                this.$refs[formName].resetFields()
-                this.$refs[upload].clearFiles()
-              }
-            )
-          }
-        }
-      )
-    },
-    onChange(file, fileList) {
-      this.form.fileList.push(file.name)
-    },
-    submitUpload() {
-      const { uploadFiles } = this.$refs['upload']
-      const form = new FormData()
-      // 在这里对每一张图片进行大小的校验，如果不符合则提示，所有不符合的都提示，校验完成后只要有不符合条件的就不执行下面的操作
-      uploadFiles.forEach(item => {
-        form.append('file', item.raw)
-      })
-      const { code, seq_id, repair_reason } = this.form
-      form.append('code', code)
-      form.append('seq_id', seq_id)
-      form.append('repair_reason', repair_reason)
-      form.append('submit_id', this.id)
-      this.$ajax({
-        method: 'post',
-        url: '/naive/submit_repair',
-        data: form
-      })
+    submitPurchase(drawerTitle) {
+      if (drawerTitle === '添加采购单') {
+        this.$refs['add'].validate(
+          (valid) => {
+            if (valid) {
+              this.$ajax('/high/add_shoplist', {
+                params: {
+                  bill_no: this.form.bill_no,
+                  submit_id: this.form.submit_id,
+                  production_line_id: this.form.production_line_id,
+                  code: this.form.code.join('|'),
+                  count: this.form.count.join('|')
+                }
+              }).then(
+                response => {
+                  const { data } = response
+                  if (data) {
+                    this.$message.success('添加成功')
+                    this.$refs['add'].resetFields()
+                    this.$refs['drawer'].closeDrawer()
+                  } else {
+                    this.$message.error('服务器错误')
+                  }
+                }
+              )
+            }
+          })
+      } else {
+        this.$refs['add'].validate(
+          (valid) => {
+            if (valid) {
+              this.$ajax('/high/update_purchase_income_submit', {
+                params: {
+                  id: this.form.id,
+                  production_line_id: this.form.production_line_id,
+                  code: this.form.code.join('|'),
+                  count: this.form.count.join('|')
+                }
+              }).then(
+                response => {
+                  if (response.data) {
+                    this.$message.success('修改成功')
+                    this.$refs['add'].resetFields()
+                    this.$refs['drawer'].closeDrawer()
+                  } else {
+                    this.$message.error('服务器错误')
+                  }
+                }
+              )
+            }
+          })
+      }
     },
     del: function(row) {
       // TODO:向后台请求删除，后台需要修改
@@ -234,9 +299,35 @@ export default {
       const codes = row.code.split('|')
       const counts = row.count.split('|')
       codes.forEach((v, i) => {
-        a.push({ label: v + ' - ' + counts[i] + '件' })
+        a.push({ label: v + ' - ' + counts[i] + '件', children: null })
       })
       return a
+    },
+    reduce(n) {
+      if (this.form.code.length > 1) {
+        this.form.code.splice(n, 1)
+        this.form.count.splice(n, 1)
+      }
+    },
+    add() {
+      if (this.form.code.length !== 4) {
+        this.form.code.push('')
+        this.form.count.push(1)
+      } else if (this.form.code.length === 4) {
+        this.$message.warning('至多选择四项工夹具')
+      }
+    },
+    clean(formName) {
+      this.form = {
+        bill_no: '',
+        submit_id: '',
+        production_line_id: '',
+        code: [''],
+        count: [1]
+      }
+    },
+    cancel(drawer) {
+      this.$refs[drawer].closeDrawer()
     }
   }
 }
