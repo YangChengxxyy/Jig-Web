971f590eeaed29d664470bfebd74d504695a8eda
YangChengxxyy <853364106@qq.com>
2020-05-19 16:07:55 +0800

    监管员 工夹具信息管理 采购审批界面

--- src/views/supervisor/jig-information/index/index.vue
+++ src/views/supervisor/jig-information/index/index.vue
@@ -1,15 +1,347 @@
 <template>
-  <div>
-    工夹具信息
-  </div>
+  <el-card class="box-card">
+    <el-form ref="form" :model="sel_form" label-width="80px" label-position="left" style="margin-top: 2%">
+      <el-row :gutter="20">
+        <el-col :span="11" :offset="1">
+          <el-form-item label="代码">
+            <el-input v-model.trim="sel_form.code" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="11">
+          <el-form-item label="名字">
+            <el-input v-model.trim="sel_form.name" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="11" :offset="1">
+          <el-form-item label="工作部门">
+            <el-select v-model.trim="sel_form.workcell_id" style="width: 100%">
+              <el-option v-for="item in workcell_list" :key="item.id" :value="item.id" :label="item.workcell" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="11">
+          <el-form-item label="用途">
+            <el-input v-model.trim="sel_form.user_for" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="11" :offset="1">
+          <el-button type="primary" @click="show_add_family = true">添加类别</el-button>
+        </el-col>
+        <el-col :span="11">
+          <el-button type="primary" icon="el-icon-search" @click="get_jig_definition_list">查询</el-button>
+          <el-button icon="el-icon-delete" @click="clear_form()">清空</el-button>
+          <el-button>导出本页</el-button>
+          <el-button>导出全部</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+    <el-divider />
+    <el-row :gutter="20" class="tac">
+      <el-col :span="6">
+        <h5 align="center"><i class="el-icon-menu" />工夹具类别</h5>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+        >
+          <el-menu-item v-for="(item,index) in jig_family_list" :id="'jig_family_'+item.family" :key="'jig_family_'+item.family" :index="index+1+''" @click="get_jig_family(item)">
+            <template slot="title">
+              <i class="el-icon-s-tools" />
+              <span>{{ item.family }}</span>
+            </template>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="18">
+        <el-table :data="jig_definition_list" border style="width: 100%">
+          <el-table-column prop="code" label="工夹具代码" width="180" />
+          <el-table-column prop="name" label="名字" width="180" />
+          <el-table-column prop="model" label="模组" width="120" />
+          <el-table-column prop="user_for" label="用途" width="240" />
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button type="primary" @click="get_jig_definition(scope.row)">编辑</el-button>
+              <el-popconfirm
+                title="确定删除该工夹具信息吗？"
+                icon-color="red"
+              >
+                <el-button slot="reference" type="danger">删除</el-button>
+              </el-popconfirm>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination
+          v-if="jig_definition_list.length !== 0"
+          style="text-align: center"
+          :current-page="page_number"
+          :page-sizes="[5, 10, 20, 30]"
+          :page-size="page_size"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="all"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </el-col>
+    </el-row>
+    <el-dialog title="编辑工夹具" :visible.sync="show_edit_dialog" width="30%" :before-close="handleClose">
+      <el-form v-if="jig_definition != null" ref="form" :model="jig_definition" label-width="150px" label-position="left">
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="工夹具代码">
+              <el-input v-model.trim="jig_definition.code" disabled />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="工夹具名字">
+              <el-input v-model.trim="jig_definition.name" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="所属部门">
+              <el-select v-model="jig_definition.workcell_id">
+                <el-option v-for="item in workcell_list" :key="item.id" :value="item.id" :label="item.workcell" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="工夹具类别">
+              <el-select v-model="jig_definition.family">
+                <el-option v-for="item in jig_family_list" :key="item.id" :value="item.id" :label="item.family" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="工夹具模组">
+              <el-select v-model="jig_definition.model">
+                <el-option v-for="item in model_list" :key="item.id" :value="item.id" :label="item.model" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="工夹具料号">
+              <el-input v-model.trim="jig_definition.part_no" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="用途">
+              <el-input v-model.trim="jig_definition.user_for" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="每条产线所需">
+              <el-input v-model.trim="jig_definition.upl" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="责任人">
+              <el-input v-model.trim="jig_definition.owner_name" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="录入人">
+              <el-input v-model.trim="jig_definition.rec_by_name" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="录入时间">
+              <el-input v-model.trim="jig_definition.rec_time" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="编辑人">
+              <el-input v-model.trim="jig_definition.edit_by_name" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="20" :offset="2">
+            <el-form-item label="编辑时间">
+              <el-input v-model.trim="jig_definition.edit_time" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <el-divider />
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="show_edit_dialog = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </el-card>
 </template>
 
 <script>
 export default {
-  name: 'Index'
+  name: 'Index',
+  data: function() {
+    return {
+      jig_family_list: [],
+      jig_family: null, // 选中的工夹具类别(1.id 2.family)
+      jig_definition_list: [], // 工夹具定义list
+      page_size: 10,
+      page_number: 1,
+      all: 0,
+      show_add_family: false, // 是否显示添加工夹具类别的dialog
+      workcell_list: [],
+      sel_form: {
+        code: '',
+        name: '',
+        workcell_id: '',
+        user_for: ''
+      },
+      show_edit_dialog: false, // 是否显示编辑工夹具定义信息的dialog
+      jig_definition: null, // 要编辑的工夹具定义信息
+      model_list: [],
+      part_no_list: []
+    }
+  },
+  computed: {
+  },
+  watch: {
+  },
+  created() {
+    this.$ajax.get('/get_workcell_list').then(
+      response => {
+        this.workcell_list = response.data
+      }
+    )
+    this.$ajax.get('/get_model_list').then(
+      response => {
+        this.model_list = response.data
+      }
+    )
+    this.$ajax.get('/get_part_no_list').then(
+      response => {
+        this.part_no_list = response.data
+      }
+    )
+    this.get_jig_family_list()
+  },
+  methods: {
+    get_jig_family_list: function() {
+      this.$ajax.get('/supervisor/get_jig_family', {
+        params: {}
+      }).then(
+        response => {
+          this.jig_family_list = response.data
+        }
+      )
+    },
+    get_jig_family: function(family) {
+      this.jig_family = family
+      this.$ajax.get('/supervisor/get_jig_definition_list', {
+        params: {
+          family_id: this.jig_family.id,
+          code: this.sel_form.code,
+          name: this.sel_form.name,
+          workcell_id: this.sel_form.workcell_id,
+          user_for: this.sel_form.user_for,
+          page_size: this.page_size,
+          page_number: this.page_number
+        }
+      }).then(
+        response => {
+          this.jig_definition_list = response.data.list
+          this.all = response.data.all
+        }
+      )
+    },
+    get_jig_definition_list: function() {
+      var family_id = ''
+      if (this.jig_family != null) {
+        family_id = this.jig_family.id
+      }
+      this.$ajax.get('/supervisor/get_jig_definition_list', {
+        params: {
+          family_id: family_id,
+          code: this.sel_form.code,
+          name: this.sel_form.name,
+          workcell_id: this.sel_form.workcell_id,
+          user_for: this.sel_form.user_for,
+          page_size: this.page_size,
+          page_number: this.page_number
+        }
+      }).then(
+        response => {
+          this.jig_definition_list = response.data.list
+          this.all = response.data.all
+        }
+      )
+    },
+    get_jig_definition: function(jig_definition) {
+      this.show_edit_dialog = true
+      this.jig_definition = jig_definition
+    },
+    clear_form: function() {
+      this.sel_form = {
+        code: '',
+        name: '',
+        workcell_id: '',
+        user_for: ''
+      }
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
+    handleSizeChange: function(val) {
+      this.page_size = val
+      this.get_jig_definition_list()
+    },
+    handleCurrentChange: function(val) {
+      this.page_number = val
+      this.get_jig_definition_list()
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .box-card{
+    width: 96%;
+    margin: 2% 2%
+  }
+  .small-box-card{
+    height: 500px;
+  }
+  .font-success{
+    color: #67C23A;
+  }
+  .font-error{
+    color: #F56C6C;
+  }
+  .font-warning{
+    color: #E6A23C;
+  }
+  .font-info{
+    color: #909399;
+  }
 </style>
--- src/views/supervisor/purchase/approve/index.vue
+++ src/views/supervisor/purchase/approve/index.vue
@@ -1,15 +1,106 @@
 <template>
-  <div>
-    采购审批
-  </div>
+  <el-card class="box-card">
+    <el-table :data="purchase_submit_list" border style="width: 100%">
+      <el-table-column prop="bill_no" label="采购单据号" width="120" />
+      <el-table-column prop="submit_name" label="采购人" width="120" />
+      <el-table-column prop="production_line_name" label="产线id" width="120" />
+      <el-table-column prop="code" label="工夹具代码" width="180">
+        <template slot-scope="scope">
+          <el-select v-model="scope.row.code_count">
+            <el-option v-for="(item,index) in scope.row.code.split('|')" :key="item" :value="item+' -- '+scope.row.count.split('|')[index]+'件'"> {{ item }} -- {{ scope.row.count.split('|')[index] }}件</el-option>
+          </el-select>
+        </template>
+      </el-table-column>
+      <el-table-column prop="submit_time" label="申请时间" width="120"/>
+      <el-table-column label="审批状态" width="120">
+        <template slot-scope="scope">
+          <div v-if="scope.row.status === '0'">待审批</div>
+          <div v-else-if="scope.row.status === '1'">初审不通过</div>
+          <div v-else-if="scope.row.status === '2'">初审通过</div>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      v-if="purchase_submit_list.length !== 0"
+      style="text-align: center"
+      :current-page="page_number"
+      :page-sizes="[5, 10, 20, 30]"
+      :page-size="page_size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="all"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </el-card>
 </template>
 
 <script>
 export default {
-  name: 'Index'
+  name: 'Index',
+  data: function() {
+    return {
+      purchase_submit_list: [],
+      code_count: '',
+      page_size: 10,
+      page_number: 1,
+      all: 0
+    }
+  },
+  computed: {
+  },
+  watch: {
+  },
+  created() {
+    this.get_purchase_submit_list()
+  },
+  methods: {
+    get_purchase_submit_list: function() {
+      this.$ajax.get('/supervisor/get_purchase_submit_list', {
+        params: {
+          page_size: this.page_size,
+          page_number: this.page_number
+        }
+      }).then(
+        response => {
+          this.purchase_submit_list = response.data.list
+          this.all = response.data.all_count
+        }
+      )
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
+    handleSizeChange: function(val) {
+      this.page_size = val
+      this.get_purchase_submit_list()
+    },
+    handleCurrentChange: function(val) {
+      this.page_number = val
+      this.get_purchase_submit_list()
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .box-card{
+    width: 96%;
+    margin: 2% 2%
+  }
+  .font-success{
+    color: #67C23A;
+  }
+  .font-error{
+    color: #F56C6C;
+  }
+  .font-warning{
+    color: #E6A23C;
+  }
+  .font-info{
+    color: #909399;
+  }
 </style>
